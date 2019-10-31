0255dc0db1816a01fc4062a7460d8533c7141d7e
Ozioma <dikep15@gmail.com>
2019-10-31 20:34:59 +0100

    new blog

---
+++ resources/views/about.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master')
+
+@push('meta')
+<meta property="og:title" content="About {{ $meta['title'] ?? config('services.meta.site_name') }}" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="{{ url()->full() }}" />
+<meta property="og:description" content="A little bit about {{ $meta['description'] ?? config('services.meta.mantra') }}" />
+@endpush
+
+@section('body')
+
+<h1>About Me</h1>
+
+<img src="{{ asset('/img/logo.svg')}}" alt="About image" class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
+
+<p class="mb-6">Hello, I am Ozioma Dike</p>
+
+<p class="mb-6">
+    Am a student and a Software developer.
+</p>
+
+
+@endsection
+
+@section('newsletter')
+<section role="main" class="flex-auto w-full container max-w-4xl mx-auto px-6">
+    @include('components.newsletter-signup')
+</section>
+@endsection
